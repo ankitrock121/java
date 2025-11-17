97e84e6edfb8b7659f6f9c6ba24505b828a6236e
AnkitRock121 <149321963+ankitrock121@users.noreply.github.com>
2025-11-17 10:07:02 +0000

    varibles.js

---
+++ varibles.js
@@ -0,0 +1,11 @@
+const name = "Ankit";
+let age = "20";
+var city = "New York";
+
+// If you try to alter the value of 'name', it will result in an error.
+// you can change age = 21; // This is allowed
+// mostly var not used because its function scoped.
+ 
+
+console.log(name,age,city)
+console.table({name,age,city}) //for print in table format
